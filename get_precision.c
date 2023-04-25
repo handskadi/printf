d8a3d344fe9e02837b8b2d1b81d0e84a358e2813
SHLKADIRI <souhailkadiri95@gmail.com>
2023-04-25 22:58:09 +0000

    precision

--- get_precision.c
+++ get_precision.c
@@ -4,8 +4,7 @@
  * get_precision - Mk - Calculates the precision for printing
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
- * @list: list of arguments.
- *
+ * @list: list of arguments
  * Return: Precision.
  */
 int get_precision(const char *format, int *i, va_list list)
@@ -39,4 +38,3 @@ int get_precision(const char *format, int *i, va_list list)
 
 	return (precision);
 }
-
